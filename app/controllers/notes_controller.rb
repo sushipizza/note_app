b0f13a3e6e268d4c9109a0e41572ead0b3e6cbf2
Ubuntu <ubuntu@ip-172-31-31-42.us-east-2.compute.internal>
2019-06-06 20:50:52 +0000

    CRU na NOTE

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,41 @@
 class NotesController < ApplicationController
+    
+    def index
+        @notes = Note.all    
+    end
+    
    def new
        @note = Note.new
+   end
+   
+   def show
+      @note = Note.find(params[:id]) 
    end
    
    def create
        #render plain: params[:note].inspect
        @note = Note.new(note_params)
-       @note.save
-       redirect_to notes_show(@note)
+       if @note.save
+           flash[:notice] = "Note saved"
+           redirect_to note_path(@note)
+       else
+           render 'new'
+       end
+   end
+   
+   def edit
+       @note = Note.find(params[:id])
+   end
+   
+   def update
+      @note = Note.find(params[:id])
+      
+      if @note.update(note_params)
+          flash[:notice] = "Note updated"
+          redirect_to note_path(@note)
+      else
+         render 'edit' 
+      end
    end
    
    private 
