7c0893b8aefcdfbe989aebfd1958e76d97ee17e3
Lukasz <lukasz.jankowski@student.wat.edu.pl>
2019-06-12 22:36:57 +0000

    logowanie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,21 @@ class SessionsController < ApplicationController
     end
     
     def create
-    
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash.now[:notice] = "zalogowano poprawnie"
+            redirect_to users_path(user)
+        else
+            flash.now[:notice] = "blad w logowaniu"
+           render 'new' 
+        end
     end
     
     def destroy
-        
+        session[:user_id] = nil
+        flash[:success] = "wylogowano"
+        redirect_to root_path
     end
     
 end
